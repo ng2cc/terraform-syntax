2ad93e82d9c4697b4016b6c3c95d4cc85ffff680
ng2c <piert2588@gmail.com>
2024-03-07 12:21:38 +0900

    subnet

--- main.tf
+++ main.tf
@@ -22,23 +22,19 @@ resource "aws_vpc" "default" {
 
 # Create a Public Subnet
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.100.0/24"
-
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.100.0/24"
+  availability_zone = "eu-west-2a"
   tags = {
     Name = "hangaramit_public_subnet_1"
   }
 }
 
-# # Create a Private Subnet
-# resource "aws_subnet" "hangaramit_private_subnet_1" {
-#   vpc_id     = aws_vpc.default.id
-#   cidr_block = "10.0.101.0/24"
-#   tags = {
-#     Name = "hangaramit_private_subnet_1"
-#   }
-# }
-
-data "aws_subnet" "private_subnet_1" {
-  id = "subnet-0c9e20da488c507d7"
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name = "hangaramit_private_subnet_1"
+  }
 }
