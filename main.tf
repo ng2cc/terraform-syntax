94b997ca651809e866928404ca38cfc108b3b4e5
ng2c <piert2588@gmail.com>
2024-03-07 11:10:56 +0900

    Create subnet

--- main.tf
+++ main.tf
@@ -16,6 +16,44 @@ provider "aws" {
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "ng2c-vpc-10"
+    Name = "main"
+  }
+}
+
+# Create a Public Subnet
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.100.0/24"
+
+  tags = {
+    Name = "hangaramit_public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.101.0/24"
+
+  tags = {
+    Name = "hangaramit_public_subnet_2"
+  }
+}
+
+# Create a private Subnet
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.110.0/24"
+
+  tags = {
+    Name = "hangaramit_private_subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.111.0/24"
+
+  tags = {
+    Name = "hangaramit_private_subnet_2"
   }
 }
