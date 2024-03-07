2503b82c47213a4ea90c0d08e72bdce66a2112af
ng2c <piert2588@gmail.com>
2024-03-07 11:46:04 +0900

    data_block

--- main.tf
+++ main.tf
@@ -29,3 +29,16 @@ resource "aws_subnet" "public_subnet_1" {
     Name = "hangaramit_public_subnet_1"
   }
 }
+
+# # Create a Private Subnet
+# resource "aws_subnet" "hangaramit_private_subnet_1" {
+#   vpc_id     = aws_vpc.default.id
+#   cidr_block = "10.0.101.0/24"
+#   tags = {
+#     Name = "hangaramit_private_subnet_1"
+#   }
+# }
+
+data "aws_subnet" "private_subnet_1" {
+  id = "subnet-0c9e20da488c507d7"
+}
